d1dfebd006014dc8b14756ef345a7e1ab6d05749
Shalabh Agarwal <sagarwal10@hotmail.com>
2019-01-08 11:11:01 -0800

    Refactor code for creating Input fields

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logoutUser } from '../../actions/authActions';
+import { clearCurrentProfile} from '../../actions/profileActions'; 
 
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
 
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   } 
 
@@ -67,12 +69,13 @@ class Navbar extends Component {
 
 Navbar.propTypes = {
   logoutUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  clearCurrentProfile: PropTypes.func.isRequired
 } 
 
 const mapStateToProps = (state) => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { logoutUser})(Navbar);
+export default connect(mapStateToProps, { clearCurrentProfile, logoutUser})(Navbar);
 
